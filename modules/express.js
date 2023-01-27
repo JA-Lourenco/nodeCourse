0c0dd1229c5821b930ccca6fd9c736818b0d390a
JA-Lourenco <ja.lourenco@outlook.com>
2023-01-26 21:01:51 -0300

    More req methods created

--- modules/express.js
+++ modules/express.js
@@ -16,6 +16,19 @@ app.get("/users", async (req, res) => {
 	}
 });
 
+app.get("/users/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+
+		const user = await UserModel.findById(id);
+
+		return res.status(200).json(user);
+	} catch (error) {
+		console.log("User find by id error: ", error);
+		res.status(500).send(error.message);
+	}
+});
+
 app.post("/users", async (req, res) => {
 	try {
 		const user = await UserModel.create(req.body);
@@ -23,6 +36,32 @@ app.post("/users", async (req, res) => {
 		res.status(201).json(user);
 	} catch (error) {
 		console.log("User creation error: ", error);
+		res.status(500).send(error.message);
+	}
+});
+
+app.patch("/users/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+
+		const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+		res.status(200).json(user);
+	} catch (error) {
+		console.log("Update User error: ", error);
+		res.status(500).send(error.message);
+	}
+});
+
+app.delete("/users/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+
+		const user = await UserModel.findByIdAndRemove(id);
+
+		res.status(200).json(user);
+	} catch (error) {
+		console.log("Delete User error: ", error);
 		res.status(500).send(error.message);
 	}
 });
